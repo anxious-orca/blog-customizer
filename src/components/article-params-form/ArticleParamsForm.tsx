62e4e08bdd1bddf41d05dd5d975ea723aadef0e5
anxious-orca <whatupmyglipglops@yandex.ru>
2025-07-24 09:33:26 +0300

    added inputs to form and connection with article's styles

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,12 +4,68 @@ import { Button } from 'src/ui/button';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
 import clsx from 'clsx';
+import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions, OptionType } from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+type TArticleParamsFormProps = {
+    articleStyles: ArticleStateType;
+	setArticleStyles: React.Dispatch<React.SetStateAction<ArticleStateType>>;
+}
+
+export const ArticleParamsForm = ({ articleStyles, setArticleStyles }: TArticleParamsFormProps) => {
+	const [formState, setFormState] = useState(articleStyles);
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleOpen = () => {
 		setIsOpen((prev) => !prev);
+	};
+
+	const handleFontChange = (option: OptionType) => {
+    setFormState((prev) => ({
+		...prev,
+		fontFamilyOption: option,
+		}));
+	};
+
+	const handleFontSizeChange = (option: OptionType) => {
+    setFormState((prev) => ({
+		...prev,
+		fontSizeOption: option,
+		}));
+	};
+
+	const handleFontColorChange = (option: OptionType) => {
+    setFormState((prev) => ({
+		...prev,
+		fontColor: option,
+		}));
+	};
+
+	const handlePageColorChange = (option: OptionType) => {
+    setFormState((prev) => ({
+		...prev,
+		backgroundColor: option,
+		}));
+	};
+
+	const handleWidthChange = (option: OptionType) => {
+    setFormState((prev) => ({
+		...prev,
+		contentWidth: option,
+		}));
+	};
+
+	const handleSubmit = (e?: React.SyntheticEvent) => {
+		e?.preventDefault();
+		setArticleStyles(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		setArticleStyles(defaultArticleState);
 	};
 
 	return (
@@ -18,9 +74,44 @@ export const ArticleParamsForm = () => {
 			<aside
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={handleFontChange}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={handleFontSizeChange}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={handleFontColorChange}
+						title='Цвет шрифта'
+					/>
+					<Separator/>
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={handlePageColorChange}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={handleWidthChange}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button title='Сбросить' htmlType='reset' type='clear' onClick={handleReset}/>
+						<Button title='Применить' htmlType='submit' type='apply' onClick={handleSubmit}/>
 					</div>
 				</form>
 			</aside>
